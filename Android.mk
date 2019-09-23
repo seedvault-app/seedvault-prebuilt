f4dac75dc0977cd38f9d343ac7491f55597bfc45
Steve Soltys <github@stevesoltys.com>
2019-09-23 02:09:46 +0000

    Travis build 158

--- Android.mk
+++ Android.mk
@@ -1,14 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := default-permissions_com.stevesoltys.backup.xml
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/default-permissions
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := permissions_com.stevesoltys.backup.xml
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_TAGS := optional
@@ -25,12 +17,6 @@ LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-
-backup_root := $(LOCAL_PATH)
-
-$(backup_root)/Backup.apk:
-	cd $(backup_root) && ./download.sh
-
 LOCAL_MODULE := Backup
 LOCAL_SRC_FILES := Backup.apk
 LOCAL_CERTIFICATE := platform
